330c33ba12d6279f1f18d8db44e29886dc2b302b
liuyaping <wangjianwei.sir@gmail.com>
2018-03-29 20:33:19 +0800

    initial

---
+++ Study_C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Study_C
+//
+//  Created by Wang Jianwei on 29/03/2018.
+//  Copyright © 2018 Wang Jianwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
